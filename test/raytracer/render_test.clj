12d407f991b62a970a9ed9b2b09cffeef02c41a4
Cowbolt <aslak.mkadmi@gmail.com>
2019-12-29 21:54:42 +0100

    Fixed test dependencies.

--- test/raytracer/render_test.clj
+++ test/raytracer/render_test.clj
@@ -1,27 +1,29 @@
 (ns raytracer.render-test
   (:require [clojure.test :refer :all]
             [raytracer.render :refer :all]
-            [raytracer.scene :refer [scene]]))
+            [raytracer.scene :refer :all]))
+
+(def mat (->Material [1.0 0.0 0.0] 0.5 0.3))
 
 (deftest ray-sphere-intersect-test
   (testing "Valid intersection yields distance"
-    (let [sphere (->Sphere [10 0 0] 3)
+    (let [sphere (->Sphere [10 0 0] 3 mat)
           ray (->Ray [0 0 0] [1 0 0])]
           (is (= (ray-sphere-intersect sphere ray) 7.0)))
-    (let [sphere (->Sphere [250 250 200] 100)
+    (let [sphere (->Sphere [250 250 200] 100 mat)
           ray (->Ray [250 250 0] [0 0 1])]
           (is (= (ray-sphere-intersect sphere ray) 100.0))))
 
   (testing "No intersection yields -1"
-    (let [sphere (->Sphere [10 10 0] 3)
+    (let [sphere (->Sphere [10 10 0] 3 mat)
           ray (->Ray [0 0 0] [1 0 0])]
           (is (= (ray-sphere-intersect sphere ray) -1.0)))
-    (let [sphere (->Sphere [250 250 200] 100)
+    (let [sphere (->Sphere [250 250 200] 100 mat)
           ray (->Ray [0 0 0] [0 0 1])]
           (is (= (ray-sphere-intersect sphere ray) -1.0))))
 
   (testing "Overlapping intersection (positive and negative) yields negative distance"
-    (let [sphere (->Sphere [0 0 0] 3)
+    (let [sphere (->Sphere [0 0 0] 3 mat)
           ray (->Ray [0 0 0] [1 0 0])]
           (is (neg? (ray-sphere-intersect sphere ray))))))
 
